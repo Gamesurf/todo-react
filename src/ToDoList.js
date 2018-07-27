e1c7197e5c9015977618f969e3857fd28c5050d2
vishnyo <vishnivecskii.a.wezom@gmail.com>
2018-07-27 21:15:36 +0300

    added revers list, fix styles

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -2,40 +2,46 @@ import React, {Component} from 'react';
 import ListItems from './ListItems';
 
 class ToDoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tasks: []
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			tasks: []
+		}
+	}
 
-    submitHandler = (event) => {
-        event.preventDefault();
+	reverse = () => {
+		this.setState(this.state.tasks.reverse())
+	};
 
-        const inputs = event.target.getElementsByClassName('formInput');
+	submitHandler = (event) => {
+		event.preventDefault();
 
-        for (let input of inputs) {
-            if (input.value.length >= 3) {
-                this.setState({tasks: [...this.state.tasks, input.value]});
-                input.value = '';
-            } else {
-                alert('Minimum three characters')
-            }
-        }
-    }
+		const inputs = event.target.getElementsByClassName('formInput');
+		const maxLength = 3;
 
-    render() {
+		for (let input of inputs) {
+			if (input.value.length >= maxLength) {
+				this.setState({tasks: [...this.state.tasks, input.value]});
+				input.value = '';
+			} else {
+				alert('Please enter ' + (maxLength - input.value.length) + ' more')
+			}
+		}
+	};
 
-        return (
-            <div className='todoList'>
-                <form className='form' onSubmit={this.submitHandler.bind(this)}>
-                    <input className='formInput form__item' type="text"/>
-                    <button className='formButton form__item' type="submit">Submit</button>
-                </form>
-                <ListItems tasks={this.state.tasks}/>
-            </div>
-        )
-    }
+	render() {
+
+		return (
+			<div className='todoList'>
+				<form className='form' onSubmit={this.submitHandler.bind(this)}>
+					<input className='formInput form__item' type="text"/>
+					<button className='formButton form__item' type="submit">Submit</button>
+				</form>
+				<div onClick={this.reverse}>Reverse</div>
+				<ListItems tasks={this.state.tasks}/>
+			</div>
+		)
+	}
 }
 
 export default ToDoList;
